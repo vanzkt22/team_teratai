1a2945a3c945a960ed396306e74aa6fcc6e1a7e0
vanzkt22 <230101014@mhs.udb.ac.id>
2025-07-20 06:36:34 +0700

    ubdete3

--- android/app/src/main/kotlin/com/example/teamteratai/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/teamteratai/MainActivity.kt
@@ -1,8 +1,6 @@
-Set-Content android\app\src\main\kotlin\com\example\teamteratai\MainActivity.kt @"
 package com.example.teamteratai
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
-"@
